cdb0638a29e9c0092240d849989a04f21a4346e7
User <User@DESKTOP-86HP7BJ>
2024-11-12 14:56:18 +0530

    changes

--- gitDemo/src/gitDemo/Test.java
+++ gitDemo/src/gitDemo/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-System.out.println("This is m1");
+System.out.println("This is main method");
 	}
 	public void m2() {
 		System.out.println("This is m2");
